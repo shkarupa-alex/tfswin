5db85f28757a3a68e4beea2e7ae0ed6ef214c534
Shkarupa Alex <shkarupa.alex@gmail.com>
2021-11-30 17:47:12 +0300

    Update version and weights

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='tfswin',
-    version='1.2.2',
+    version='2.0.0',
     description='Keras (TensorFlow v2) reimplementation of Swin Transformer model.',
     long_description=long_description,
     long_description_content_type="text/markdown",
