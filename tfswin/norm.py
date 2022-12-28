8034ce55fd7626c90f009f8c18507776f1859670
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-12-28 22:46:19 +0300

    Drop old comment

--- tfswin/norm.py
+++ tfswin/norm.py
@@ -30,24 +30,3 @@ class LayerNorm(layers.LayerNormalization):
                 f'Fused layer normalization is not supported for axis {self.axis} and inputs rank {ndims}.')
 
         return True
-
-    # def _fused_can_be_used(self, ndims):
-    #     """Returns false if fused implementation cannot be used.
-    #
-    #     Check if the axis is contiguous and can be collapsed into the last axis.
-    #     The self.axis is assumed to have no duplicates.
-    #     """
-    #     axis = sorted(self.axis)
-    #     can_use_fused = False
-    #
-    #     if axis[-1] == ndims - 1 and axis[-1] - axis[0] == len(axis) - 1:
-    #         can_use_fused = True
-    #
-    #     # fused_batch_norm will silently raise epsilon to be at least 1.001e-5,
-    #     # so we cannot used the fused version if epsilon is below that value.
-    #     # Also, the variable dtype must be float32, as fused_batch_norm only
-    #     # supports float32 variables.
-    #     if self.epsilon < 1.001e-5 or self.dtype != "float32":
-    #         can_use_fused = False
-    #
-    #     return can_use_fused
