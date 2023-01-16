a84c9a64a3c01d4bf0493bab8cb1ecd73f567531
Shkarupa Alex <shkarupa.alex@gmail.com>
2023-01-16 16:43:07 +0300

    Revert wrongly committed changes

--- tfswin/tests/test_model.py
+++ tfswin/tests/test_model.py
@@ -8,92 +8,27 @@ from tensorflow.python.util import object_identity
 from tensorflow.python.training.tracking import util as trackable_util
 
 
-# @test_combinations.run_all_keras_modes
-# class TestModelV1(test_combinations.TestCase):
-#     def setUp(self):
-#         super(TestModelV1, self).setUp()
-#         self.default_policy = mixed_precision.global_policy()
-#
-#     def tearDown(self):
-#         super(TestModelV1, self).tearDown()
-#         mixed_precision.set_global_policy(self.default_policy)
-#
-#     @parameterized.parameters((False,), (True,))
-#     def test_train(self, use_fp16):
-#         if use_fp16:
-#             mixed_precision.set_global_policy('mixed_float16')
-#
-#         model = SwinTransformerTiny224(weights=None)
-#         model.compile(optimizer='rmsprop', loss='mse', run_eagerly=test_utils.should_run_eagerly())
-#
-#         images = np.random.random((10, 224, 224, 3)).astype('float32')
-#         labels = (np.random.random((10, 1)) + 0.5).astype('int32')
-#         model.fit(images, labels, epochs=1, batch_size=2)
-#
-#         # test config
-#         model.get_config()
-#
-#         # check whether the model variables are present in the trackable list of objects
-#         checkpointed_objects = object_identity.ObjectIdentitySet(trackable_util.list_objects(model))
-#         for v in model.variables:
-#             self.assertIn(v, checkpointed_objects)
-#
-#     def test_ape(self):
-#         model = SwinTransformerTiny224(weights=None, use_ape=True)
-#         model.compile(optimizer='rmsprop', loss='mse', run_eagerly=test_utils.should_run_eagerly())
-#
-#         images = np.random.random((10, 224, 224, 3)).astype('float32')
-#         labels = (np.random.random((10, 1)) + 0.5).astype('int32')
-#         model.fit(images, labels, epochs=1, batch_size=2)
-#
-#         # test config
-#         model.get_config()
-#
-#         # check whether the model variables are present in the trackable list of objects
-#         checkpointed_objects = object_identity.ObjectIdentitySet(trackable_util.list_objects(model))
-#         for v in model.variables:
-#             self.assertIn(v, checkpointed_objects)
-#
-#     def test_var_shape(self):
-#         model = SwinTransformerTiny224(weights=None, include_top=False, input_shape=(None, None, 3))
-#         run_eagerly = test_utils.should_run_eagerly()
-#         model.compile(optimizer='rmsprop', loss='mse', run_eagerly=run_eagerly, jit_compile=not run_eagerly)
-#
-#         images = np.random.random((10, 512, 384, 3)).astype('float32')
-#         labels = (np.random.random((10, 16, 12, 768)) + 0.5).astype('int32')
-#         model.fit(images, labels, epochs=1, batch_size=2)
-#
-#         # test config
-#         model.get_config()
-#
-#         # check whether the model variables are present in the trackable list of objects
-#         checkpointed_objects = object_identity.ObjectIdentitySet(trackable_util.list_objects(model))
-#         for v in model.variables:
-#             self.assertIn(v, checkpointed_objects)
-
-
-# @test_combinations.run_all_keras_modes
-class TestModelV2(test_combinations.TestCase):
+@test_combinations.run_all_keras_modes
+class TestModelV1(test_combinations.TestCase):
     def setUp(self):
-        super(TestModelV2, self).setUp()
+        super(TestModelV1, self).setUp()
         self.default_policy = mixed_precision.global_policy()
 
     def tearDown(self):
-        super(TestModelV2, self).tearDown()
+        super(TestModelV1, self).tearDown()
         mixed_precision.set_global_policy(self.default_policy)
 
-    @parameterized.parameters((False,),)
-    # @parameterized.parameters((False,), (True,))
+    @parameterized.parameters((False,), (True,))
     def test_train(self, use_fp16):
         if use_fp16:
             mixed_precision.set_global_policy('mixed_float16')
 
-        model = SwinTransformerV2Tiny256(weights=None)
-        model.compile(optimizer='rmsprop', loss='mse', run_eagerly=True)
+        model = SwinTransformerTiny224(weights=None)
+        model.compile(optimizer='rmsprop', loss='mse', run_eagerly=test_utils.should_run_eagerly())
 
-        images = np.random.random((10, 256, 256, 3)).astype('float32')
+        images = np.random.random((10, 224, 224, 3)).astype('float32')
         labels = (np.random.random((10, 1)) + 0.5).astype('int32')
-        model.fit(images, labels, epochs=1, batch_size=10)
+        model.fit(images, labels, epochs=1, batch_size=2)
 
         # test config
         model.get_config()
@@ -103,24 +38,86 @@ class TestModelV2(test_combinations.TestCase):
         for v in model.variables:
             self.assertIn(v, checkpointed_objects)
 
-        assert False
+    def test_ape(self):
+        model = SwinTransformerTiny224(weights=None, use_ape=True)
+        model.compile(optimizer='rmsprop', loss='mse', run_eagerly=test_utils.should_run_eagerly())
 
-    # def test_var_shape(self):
-    #     model = SwinTransformerV2Tiny256(weights=None, include_top=False, input_shape=(None, None, 3))
-    #     run_eagerly = test_utils.should_run_eagerly()
-    #     model.compile(optimizer='rmsprop', loss='mse', run_eagerly=run_eagerly, jit_compile=not run_eagerly)
-    #
-    #     images = np.random.random((10, 512, 384, 3)).astype('float32')
-    #     labels = (np.random.random((10, 16, 12, 768)) + 0.5).astype('int32')
-    #     model.fit(images, labels, epochs=1, batch_size=2)
-    #
-    #     # test config
-    #     model.get_config()
-    #
-    #     # check whether the model variables are present in the trackable list of objects
-    #     checkpointed_objects = object_identity.ObjectIdentitySet(trackable_util.list_objects(model))
-    #     for v in model.variables:
-    #         self.assertIn(v, checkpointed_objects)
+        images = np.random.random((10, 224, 224, 3)).astype('float32')
+        labels = (np.random.random((10, 1)) + 0.5).astype('int32')
+        model.fit(images, labels, epochs=1, batch_size=2)
+
+        # test config
+        model.get_config()
+
+        # check whether the model variables are present in the trackable list of objects
+        checkpointed_objects = object_identity.ObjectIdentitySet(trackable_util.list_objects(model))
+        for v in model.variables:
+            self.assertIn(v, checkpointed_objects)
+
+    def test_var_shape(self):
+        model = SwinTransformerTiny224(weights=None, include_top=False, input_shape=(None, None, 3))
+        run_eagerly = test_utils.should_run_eagerly()
+        model.compile(optimizer='rmsprop', loss='mse', run_eagerly=run_eagerly, jit_compile=not run_eagerly)
+
+        images = np.random.random((10, 512, 384, 3)).astype('float32')
+        labels = (np.random.random((10, 16, 12, 768)) + 0.5).astype('int32')
+        model.fit(images, labels, epochs=1, batch_size=2)
+
+        # test config
+        model.get_config()
+
+        # check whether the model variables are present in the trackable list of objects
+        checkpointed_objects = object_identity.ObjectIdentitySet(trackable_util.list_objects(model))
+        for v in model.variables:
+            self.assertIn(v, checkpointed_objects)
+
+
+@test_combinations.run_all_keras_modes
+class TestModelV2(test_combinations.TestCase):
+    def setUp(self):
+        super(TestModelV2, self).setUp()
+        self.default_policy = mixed_precision.global_policy()
+
+    def tearDown(self):
+        super(TestModelV2, self).tearDown()
+        mixed_precision.set_global_policy(self.default_policy)
+
+    @parameterized.parameters((False,), (True,))
+    def test_train(self, use_fp16):
+        if use_fp16:
+            mixed_precision.set_global_policy('mixed_float16')
+
+        model = SwinTransformerV2Tiny256(weights=None)
+        model.compile(optimizer='rmsprop', loss='mse', run_eagerly=test_utils.should_run_eagerly())
+
+        images = np.random.random((10, 256, 256, 3)).astype('float32')
+        labels = (np.random.random((10, 1)) + 0.5).astype('int32')
+        model.fit(images, labels, epochs=1, batch_size=2)
+
+        # test config
+        model.get_config()
+
+        # check whether the model variables are present in the trackable list of objects
+        checkpointed_objects = object_identity.ObjectIdentitySet(trackable_util.list_objects(model))
+        for v in model.variables:
+            self.assertIn(v, checkpointed_objects)
+
+    def test_var_shape(self):
+        model = SwinTransformerV2Tiny256(weights=None, include_top=False, input_shape=(None, None, 3))
+        run_eagerly = test_utils.should_run_eagerly()
+        model.compile(optimizer='rmsprop', loss='mse', run_eagerly=run_eagerly, jit_compile=not run_eagerly)
+
+        images = np.random.random((10, 512, 384, 3)).astype('float32')
+        labels = (np.random.random((10, 16, 12, 768)) + 0.5).astype('int32')
+        model.fit(images, labels, epochs=1, batch_size=2)
+
+        # test config
+        model.get_config()
+
+        # check whether the model variables are present in the trackable list of objects
+        checkpointed_objects = object_identity.ObjectIdentitySet(trackable_util.list_objects(model))
+        for v in model.variables:
+            self.assertIn(v, checkpointed_objects)
 
 
 if __name__ == '__main__':
